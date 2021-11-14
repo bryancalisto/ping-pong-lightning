05ddf69f3493e84e3d038796fd068ebc233a8e48
Bryan Calisto <bryancalisto@yahoo.es>
2021-11-14 16:24:59 -0500

    Refactor: Changed button style

--- lib/views/home_vw.dart
+++ lib/views/home_vw.dart
@@ -21,14 +21,26 @@ class HomeVw extends StatelessWidget {
               },
               closedBuilder: (BuildContext context, void Function() action) {
                 return TextButton(
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.resolveWith((_) => 100),
+                    shape: MaterialStateProperty.resolveWith<OutlinedBorder>(
+                      (_) => const RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.grey, style: BorderStyle.solid, width: 2),
+                      ),
+                    ),
+                  ),
                   onPressed: action,
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'START GAME',
-                      style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width * 0.06,
-                        letterSpacing: MediaQuery.of(context).size.width * 0.008,
+                    child: Transform(
+                      transform: Matrix4.identity()..rotateX(0.4), // Give old school effect
+                      child: Text(
+                        'START GAME',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: MediaQuery.of(context).size.width * 0.06,
+                          letterSpacing: MediaQuery.of(context).size.width * 0.008,
+                        ),
                       ),
                     ),
                   ),
